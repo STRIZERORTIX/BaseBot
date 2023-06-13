e0e639908e4e309acc5c95852d807d213fa29f6f
STRIZERORTIX <abhi.varadan@gmail.com>
2023-06-13 11:53:46 +0530

    updated the basebot

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
-import Hero from "./components/hero.jsx";
-import Demo from "./components/demo.jsx";
+import Hero from "./Components/hero.jsx";
+import Demo from "./Components/demo.jsx";
 import "./app.css";
 
 const App = () => {
